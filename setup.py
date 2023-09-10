7dd8363abf10716f2bad273e1d02e735fbc3012f
Alex Boisvert <boisvert42@gmail.com>
2023-09-10 13:49:08 -0700

    Dropquote fix 2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypuz',
-    version='0.1.3',
+    version='2023.09.10.1',
     author='Alex Boisvert et al.',
     author_email='crosswordnexus@gmail.com',
     packages=find_packages(),
